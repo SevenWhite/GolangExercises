99700afd818aee19d8035961f236cf9aee42102c
SevenWhite <album.777.leo@gmail.com>
2016-09-14 14:46:10 +0300

    update cf

--- cf/main.go
+++ cf/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-//"github.com/SevenWhite/GolangExercises/tempconv"
+	"fmt"
+	"github.com/SevenWhite/GolangExercises/tempconv"
+	"os"
+	"strconv"
 )
 
 func main() {
-	//tempconv
+	numbers := os.Args[1:]
+
+	for _, number := range numbers {
+		number, err := strconv.ParseFloat(number, 64)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Incorrect number", err)
+			continue
+		}
+
+		c := tempconv.Celsius(number)
+		f := tempconv.Fahrenheit(number)
+		k := tempconv.Kelvin(number)
+
+		fmt.Printf("%s = %s = %s\n", c, tempconv.CToF(c), tempconv.CToK(c))
+		fmt.Printf("%s = %s = %s\n", f, tempconv.FToC(f), tempconv.FToK(f))
+		fmt.Printf("%s = %s = %s\n", k, tempconv.KToC(k), tempconv.KToF(k))
+	}
 }
