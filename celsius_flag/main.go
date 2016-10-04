302d0a060c4dab6a6fb47fdf20cd04a613c93e99
SevenWhite <album.777.leo@gmail.com>
2016-10-04 10:21:45 +0300

    add sleep and celsius_flag (work with flags package)

---
+++ celsius_flag/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Celsius float64
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+type CelsiusFlag struct {
+	Celsius
+}
+
+func (f *CelsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	}
+	return fmt.Errorf("Wrong temperature %q", s)
+}
+
+func CreateCelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := CelsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
+
+var temperature = CreateCelsiusFlag("t", Celsius(23), "temperature (Celsius)")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("\nTemperature: %v", temperature)
+}
